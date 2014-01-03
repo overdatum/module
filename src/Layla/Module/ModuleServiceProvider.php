9742006cf45a920a4390bef724446268b8265439
Koen Schmeets <k.schmeets@gmail.com>
2014-01-03 18:28:00 +0100

    Adding missing routes

--- src/Layla/Module/ModuleServiceProvider.php
+++ src/Layla/Module/ModuleServiceProvider.php
@@ -43,6 +43,11 @@ class ModuleServiceProvider extends ServiceProvider {
 
 		$route->resource('api/modules', 'Layla\Module\Controllers\Api\ModuleController');
 		$route->resource('api/resources', 'Layla\Module\Controllers\Api\ResourceController');
+		$route->resource('api/columns', 'Layla\Module\Controllers\Api\ColumnsController');
+		$route->resource('api/fields', 'Layla\Module\Controllers\Api\FieldsController');
+		$route->resource('api/forms', 'Layla\Module\Controllers\Api\FormsController');
+		$route->resource('api/relations', 'Layla\Module\Controllers\Api\RelationsController');
+		$route->resource('api/tabs', 'Layla\Module\Controllers\Api\TabsController');
 	}
 
 	/**
