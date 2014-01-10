eb2c2e4a976a58a9be73fe6921cf5924e0fcad19
Koen Schmeets <k.schmeets@gmail.com>
2014-01-11 00:05:45 +0100

    Updating helpers

--- src/helpers.php
+++ src/helpers.php
@@ -2,18 +2,32 @@
 
 function eval_blade($file, $data)
 {
-	extract($data);
+    extract($data);
 
-	$content = file_get_contents($file);
-	$compiled = Blade::compileString($content);
+    $content = file_get_contents($file);
+    $compiled = Blade::compileString($content);
 
-	ob_start();
-	eval('?>'.$compiled);
+    ob_start();
+    eval('?>'.$compiled);
 
-	return ob_get_clean();
+    return ob_get_clean();
 }
 
 function layla_module_stubs_path()
 {
 	return __DIR__.'/stubs/';
+}
+
+if ( ! function_exists('de'))
+{
+	/**
+	 * Escape HTML entities in a string.
+	 *
+	 * @param  string  $value
+	 * @return string
+	 */
+	function de($value)
+	{
+		return html_entity_decode($value, ENT_QUOTES, 'UTF-8');
+	}
 }
