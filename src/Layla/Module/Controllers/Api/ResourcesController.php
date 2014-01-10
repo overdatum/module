4b9913edcd7264a80f175cf27c67bd268aea2027
Koen Schmeets <k.schmeets@gmail.com>
2014-01-11 00:00:12 +0100

    Rename ResourceController => ResourcesController

---
+++ src/Layla/Module/Controllers/Api/ResourcesController.php
@@ -0,0 +1,62 @@
+<?php namespace Layla\Module\Controllers\Api;
+
+use Layla\Module\Controllers\Api\BaseController;
+
+use Layla\Module\Models\Resource;
+
+class ResourcesController extends BaseController {
+
+    /**
+     * Create a new ResourceController instance.
+     *
+     * @param  \Layla\Module\Models\Resource  $model
+     * @return void
+     */
+    public function __construct(Resource $model)
+    {
+        $this->model = $model;
+    }
+
+    /**
+     * Scopes to execute by default
+     *
+     * @var array
+     */
+    protected $scopes = array();
+
+    /**
+     * The relations to eager load on every query.
+     *
+     * @var array
+     */
+    protected $with = array();
+
+    /**
+     * Validation rules used when getting a list of resources
+     *
+     * @var array
+     */
+    protected $indexRules = array();
+
+    /**
+     * Validation rules used when storing a resource
+     *
+     * @var array
+     */
+    protected $storeRules = array();
+
+    /**
+     * Validation rules used when getting a resource
+     *
+     * @var array
+     */
+    protected $showRules = array();
+
+    /**
+     * Validation rules used when updating a resource
+     *
+     * @var array
+     */
+    protected $updateRules = array();
+
+}
