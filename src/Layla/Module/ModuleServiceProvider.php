c06f0a85855e889f5b7defd76fdf8c2d1eeb5be6
Koen Schmeets <k.schmeets@gmail.com>
2014-01-11 01:06:41 +0100

    Updating ServiceProvider

--- src/Layla/Module/ModuleServiceProvider.php
+++ src/Layla/Module/ModuleServiceProvider.php
@@ -1,6 +1,7 @@
 <?php namespace Layla\Module;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
 
 class ModuleServiceProvider extends ServiceProvider {
 
@@ -42,13 +43,25 @@ class ModuleServiceProvider extends ServiceProvider {
 	{
 		$route = $this->app->make('router');
 
+		// Api routes
 		$route->resource('api/modules', 'Layla\Module\Controllers\Api\ModuleController');
-		$route->resource('api/resources', 'Layla\Module\Controllers\Api\ResourceController');
+		$route->resource('api/resources', 'Layla\Module\Controllers\Api\ResourcesController');
 		$route->resource('api/columns', 'Layla\Module\Controllers\Api\ColumnsController');
 		$route->resource('api/fields', 'Layla\Module\Controllers\Api\FieldsController');
 		$route->resource('api/forms', 'Layla\Module\Controllers\Api\FormsController');
 		$route->resource('api/relations', 'Layla\Module\Controllers\Api\RelationsController');
 		$route->resource('api/tabs', 'Layla\Module\Controllers\Api\TabsController');
+
+		// Resource routes
+		$route->resource('modules', 'Layla\Module\Controllers\ModulesController');
+		$route->resource('resources', 'Layla\Module\Controllers\ResourcesController');
+		$route->resource('wizard', 'Layla\Module\Controllers\WizardController');
+
+		// Application routes
+		$route->get('module/{any?}/{thing?}', function()
+		{
+			return View::make('module::layout');
+		});
 	}
 
 	/**
